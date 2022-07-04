99f242b0402b408e08010093b1a33cb7107ce669
daniel mbuthia <danielwandurua@gmail.com>
2022-07-04 20:34:51 +0000

    clean the code

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ stages = ['''
 =========
 ''']
 lives = 6
-game_ends = False
+
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
@@ -67,24 +67,21 @@ display = []
 for _ in range(word_length):
     display += "_"
     
-while "_" in display and not game_ends and lives > 0: 
+while "_" in display and lives >= 0: 
     guess = input("Guess a letter: ").lower()
     for index,item in enumerate(chosen_word):
         if guess == item:
             display[index] =item
-            
+    print(display)
     if guess not in chosen_word:
         print(stages[lives])
         lives -=1
-        if lives == 0:
-            game_ends = True
+        if lives < 0:
+            print("You lose")
     if "_" not in display:
-        game_ends = True
-    print(display)
-    if lives == 0 and "_" in display:
-        print("You lose")
-    elif game_ends:
         print("You won")
+        
+        
     
     
     
